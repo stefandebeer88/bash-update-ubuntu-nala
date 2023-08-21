0d8df568d5758c7205dd6f95410bfba236c512a0
stefandebeer88 <stefandebeer85@gmail.com>
2023-08-21 10:01:25 +0200

    The file that needs to be run.

--- update-ubuntu-using-nala.sh
+++ update-ubuntu-using-nala.sh
@@ -5,8 +5,19 @@
 # Description: This script will use Nala to update a Ubuntu system. I use this as part of my dailies to ensure that my system is up to date.
 # Prerequisites: Nala must be installed. You can see how to install it here: https://adamtheautomator.com/nala-apt/
 # eMail: stefandebeer85@gmail.com
+
 GREEN="\033[0;32m" # Green
 NC="\033[0m" # No Color
+
+# Check if Nala is already installed, if not then install Nala.
+if ! command -v nala &> /dev/null; then
+    echo "Nala is not installed. Installing Nala..."
+    sudo apt update
+    sudo apt install nala
+else
+    echo "Nala is already installed."
+fi
+
 printf "\n\n"
 printf "Starting ${GREEN}APT Update${NC}!\n"
 printf "\n\n"
